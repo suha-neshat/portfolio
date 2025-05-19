776a7a5d9830410fbac34c7a4e895d2ed877fcd1
vanbellt <neshat.suha@ryerson.ca>
2025-05-19 18:28:26 -0400

    Fix case mismatch on Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
